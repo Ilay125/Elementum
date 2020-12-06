df21fa94330d5aed9251b601f90ad9b66635dd6b
Ilay <ilaybl125@gmail.com>
2020-12-06 21:38:00 +0200

    Added Shape Forge

--- src/main/java/com/ri/elementum/util/RegistryHandler.java
+++ src/main/java/com/ri/elementum/util/RegistryHandler.java
@@ -1,8 +1,11 @@
 package com.ri.elementum.util;
 
+import com.ri.elementum.blocks.BlockItemBase;
+import com.ri.elementum.blocks.ShapeForgeBlock;
 import com.ri.elementum.Elementum;
 import com.ri.elementum.items.ItemBase;
 import net.minecraft.item.Item;
+import net.minecraft.block.Block;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
@@ -11,9 +14,11 @@ import net.minecraftforge.registries.ForgeRegistries;
 
 public class RegistryHandler {
     public static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS, Elementum.MOD_ID);
+    public static final DeferredRegister<Block> BLOCKS = new DeferredRegister<>(ForgeRegistries.BLOCKS, Elementum.MOD_ID);
 
     public static void init() {
         ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+        BLOCKS.register(FMLJavaModLoadingContext.get().getModEventBus());
     }
 
     //this is items you moron
@@ -24,4 +29,10 @@ public class RegistryHandler {
     public static final RegistryObject<Item> DEEP_STONE = ITEMS.register("deep_stone", ItemBase::new);
     public static final RegistryObject<Item> AIR_GEM = ITEMS.register("air_gem", ItemBase::new);
 
+    //fucking blocks idot
+    public static final RegistryObject<Block> SHAPE_FORGE_BLOCK = BLOCKS.register("shape_forge_block", ShapeForgeBlock::new);
+
+
+    //blocks items??? wtf the fuck
+    public static final RegistryObject<Item> SHAPE_FORGE_ITEM = ITEMS.register("shape_forge_block", () -> new BlockItemBase(SHAPE_FORGE_BLOCK.get()));
 }
